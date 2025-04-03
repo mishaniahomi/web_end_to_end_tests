bec6c01355f68db4de8aa579ce612049b1339f28
homi <misha.homi@gmail.com>
2025-04-03 21:30:07 +0300

    Add function is_element_present and add new test for input username

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,3 +6,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "button[type=submit]")
         login_link.click()
+
+    def should_be_input_username(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "input#username"), "Input for username is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,33 @@
 from pages.main_page import MainPage
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+from dotenv import load_dotenv
 
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 
 def test_guest_can_go_to_login_page():
-    link = "https://dev-cab.dev-ai-melanoma.ru/login"
     cService = webdriver.ChromeService(executable_path=ChromeDriverManager().install())
-    browser = webdriver.Chrome(service = cService)
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    browser = webdriver.Chrome(service=cService)
+    base_url = os.environ.get("BASE_URL")
+    link = base_url + "/login"
+    page = MainPage(
+        browser, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
+
+def test_quest_should_be_input_username():
+    cService = webdriver.ChromeService(executable_path=ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=cService)
+    base_url = os.environ.get("BASE_URL")
+    link = base_url + "/login"
+    page = MainPage(
+        browser, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.should_be_input_username()  # выполняем метод страницы — переходим на страницу логина
